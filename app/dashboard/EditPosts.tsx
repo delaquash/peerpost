269a891594bab2c8197b66552802bb4a3e86f5e0
delaquash <olaide1191@gmail.com>
2023-04-19 15:24:50 +0100

    feat: add delete post button

--- app/dashboard/EditPosts.tsx
+++ app/dashboard/EditPosts.tsx
@@ -1,4 +1,8 @@
+import axios from "axios";
 import Image from "next/image";
+import { useState } from "react";
+import { useMutation } from "react-query";
+import Toggle from "./toggle";
 
 type EditProps = {
   id: string;
@@ -19,6 +23,23 @@ export default function EditPost({
   comments,
   id,
 }: EditProps) {
+  const [toggle, setToggle] = useState<boolean>(false);
+  // delete post
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id }),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+      onSuccess: (data) => {
+        console.log(data);
+      },
+    }
+  );
+  const deletePost = () => {
+    mutate(id);
+  };
   return (
     <>
       <div className="bg-white my-8 p-8 rounded-lg">
@@ -27,13 +48,16 @@ export default function EditPost({
           <h3>{name}</h3>
         </div>
         <div className="my-8">
-            <p className="break-all">{title}</p>
+          <p className="break-all">{title}</p>
         </div>
         <div className="flex items-center gap-4">
-            <p className="text-sm font-bold text-gray-700">{comments?.length} comment</p>
-            <button className="text-red-500 font-bold text-sm">Delete</button>
+          <p className="text-sm font-bold text-gray-700">
+            {comments?.length} comment
+          </p>
+          <button className="text-red-500 font-bold text-sm">Delete</button>
         </div>
       </div>
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
     </>
   );
 }
