66b7ba7d71332ffe64b0eccca8f127f8a2bcab08
delaquash <olaide1191@gmail.com>
2023-04-19 16:14:19 +0100

    fixed: delete post route

--- pages/api/post/deletePost.ts
+++ pages/api/post/deletePost.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 import prisma from "../../../prisma/client";
-// import { authOptions } from "../auth/[...nextauth]";
 import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(
