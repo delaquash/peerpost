d70d44787c2431e044d3eb7c7a7b1f4815e5fbef
delaquash <olaide1191@gmail.com>
2023-04-20 12:02:12 +0100

    feat: add type safety for post

--- pages/api/post/[details].ts
+++ pages/api/post/[details].ts
@@ -1,18 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth/next";
 import prisma from "../../../prisma/client";
-// import { authOptions } from "../auth/[...nextauth]";
-import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-    if(req.method === "GET"){
-        try {
-            
-        } catch (error) {
-            res.
-        }
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      const data = await prisma.post.findUnique({
+        where: {
+          id: req.query.details,
+        },
+        include: {
+          user: true,
+          Comment: {
+            orderBy: {
+              createdAt: "desc",
+            },
+            include: {
+              user: true,
+            },
+          },
+        },
+      });
+      return res.status(200).json(data);
+    } catch (error) {
+      res.status(403).json({ error: "Error occured.Kindly try again." });
     }
   }
+}
